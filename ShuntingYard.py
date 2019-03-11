1debdb8d0b674e68b6454df8f40fcbe69950ccf6
Michael Coleman <michaelcoleman135@gmail.com>
2019-03-11 21:03:48 +0000

    Set Up variables

--- ShuntingYard.py
+++ ShuntingYard.py
@@ -1,7 +1,14 @@
 # Michael Coleman
 # Shunting Yard Algorithm
 
-
+# function shunt
+def shunt(infix):
+    
+    #set up variables
+    specials = {'*' : 50,'.' : 40,'|' : 30}
+    
+    pofix = ""
+    stack = ""
 
 
             
