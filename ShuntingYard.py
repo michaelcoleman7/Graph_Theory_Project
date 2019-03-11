e70b52261c8de40239876bb9ac2e2f99137c60b3
Michael Coleman <michaelcoleman135@gmail.com>
2019-03-11 21:07:05 +0000

    added for loop to handle brackets in expressions

--- ShuntingYard.py
+++ ShuntingYard.py
@@ -9,6 +9,18 @@ def shunt(infix):
     
     pofix = ""
     stack = ""
+    
+    #for loop running through each character in infix
+    for c in infix:
+        #if character = ( , then add to stack
+        if c == '(':
+            stack = stack + c
+        # if character = ) , then remove each item on stack until ( is encountered
+        elif c == ')':
+            while stack[-1] != '('
+                pofix ,stack = pofix + stack[-1], stack[:-1]
+            # remove the last character again to remove the ) from the stack
+            stack = stack[:-1]
 
 
             
