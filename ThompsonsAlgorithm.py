5eb531227f26caf24b34b38031bf5fd9bad9bf65
Michael Coleman <michaelcoleman135@gmail.com>
2019-03-12 12:27:33 +0000

    Added Thompson's Algorithm file and set up classes

---
+++ ThompsonsAlgorithm.py
@@ -0,0 +1,22 @@
+# Michael Coleman
+# Thompson's Algorithm
+
+class state:
+    label = None
+    edge1 = None
+    edge2 = None
+    
+class nfa:
+    initial = None
+    accept = None
+    
+    def __init__(self, initial, accept):
+        self.initial = initial
+        self.accept = accept
+    
+
+
+
+
+
+            
