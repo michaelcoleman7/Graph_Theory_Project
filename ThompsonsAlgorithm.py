014580dd524a5b87e22a628d197f8aaca5e8bc03
Michael Coleman <michaelcoleman135@gmail.com>
2019-03-12 12:57:21 +0000

    Added if statement to deal with '|'

--- ThompsonsAlgorithm.py
+++ ThompsonsAlgorithm.py
@@ -27,6 +27,21 @@ def compile(pofix):
             # Push NFA to the stack
             nfastack.append(nfa.initial, nfa2.accept)
         elif c =='|':
+            # Pop two NFA's off the stack
+            nfa2 = nfastack.pop()
+            nfa1 = nfastack.pop()
+            #Create a new initial state, connect it to initial state
+            # of the two NFA's popped from the stack
+            initial = state()
+            initial.edge1 = nfa1.initial
+            intiial.edge2 = nfa2.initial
+            #create a new accept state, connecting the accept states
+            # of the two NFA's popped from the stack, to the new state
+            accept = state()
+            nfa1.accept.edge1 = accept
+            nfa2.accept.edge2 = accept
+            # push new NFA to the stack
+            nfastack.append(nfa(initial, accept))
         elif c =='*':
         else:
             accept = state()
