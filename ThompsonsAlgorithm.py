d175e92ffa4f807a71059d87ebe76dc5a61c5866
Michael Coleman <michaelcoleman135@gmail.com>
2019-03-12 12:44:39 +0000

    Added if statement to deal with '.'

--- ThompsonsAlgorithm.py
+++ ThompsonsAlgorithm.py
@@ -13,6 +13,27 @@ class nfa:
     def __init__(self, initial, accept):
         self.initial = initial
         self.accept = accept
+        
+def compile(pofix):
+    nfastack = []
+    
+    for c in pofix:
+        if c == '.':
+            # Pop two NFA's off stack
+            nfa2 = nfastack.pop()
+            nfa1 = nfastack.pop()
+            # Connect first NFA's accept state to the seconds initial
+            nfa1.accept.edge1 = nfa2.initial
+            # Push NFA to the stack
+            nfastack.append(nfa.initial, nfa2.accept)
+        elif c =='|':
+        elif c =='*':
+        else:
+            accept = state()
+            initial = state()
+            initial.label = c
+            initial.edge1 = accept
+            nfastack.append(nfa(initial, accept))
     
 
 
